76cb723635d05a1d1e7e1397dce2310b60487c1a
dudueduardo121 <dudueduardo121@gmail.com>
2020-08-20 15:29:20 -0300

    component modal cart

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,15 +17,31 @@ export default class Modal extends Component {
                         return null;
                     }
                     else{
+                      return (  
                         <ModalContainer>
                             <div className="container">
                                 <div className="row">
-                                    <div className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
+                                    <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
                                         <h5>item add to the cart</h5>
+                                        <img src={img} classname="img-fluid" alt="product image"/>
+                                        <h5>{title}</h5>
+                                        <h5 className="text-muted">price : $ {price}</h5>
+                                        <Link to="/">
+                                            <ButtoContainer onClick={()=>closeModal()}>
+                                                store
+                                            </ButtoContainer>
+                                        </Link>
+
+                                        <Link to="/cart">
+                                            <ButtoContainer cart onClick={()=>closeModal()}>
+                                                go to cart
+                                            </ButtoContainer>
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
                         </ModalContainer>
+                      );
                     }
                      
                 }}
@@ -35,6 +51,18 @@ export default class Modal extends Component {
 }
 
 const ModalContainer = styled.div`
+    position:fixed;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    background:rgba(0,0,0,0.3);
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    #modal{
+        background:var(--mainWhite);
+    }
 
-`
+`;
 
