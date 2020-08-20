d07eb6181607affc596a2cdc8147d4fcb9fbfefd
dudueduardo121 <dudueduardo121@gmail.com>
2020-08-19 21:53:40 -0300

    products componets

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        shooping.com
-      </footer>
-    </div>
-  );
+import Navbar from './components/Navbar';
+//import Product from './components/Product';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <Navbar/>
+        <Switch>
+          <Route path="/" component={ ProductList } exact/>
+          <Route path="/details" component={ Details }/>
+          <Route path="/cart" component={ Cart }/>
+          <Route component={ Default }/>
+        </Switch>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
